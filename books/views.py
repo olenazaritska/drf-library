38b3a572077c8a38712daff3418fc16f3860d03a
Olena Zaritska <zaritskaos@gmail.com>
2024-11-01 18:57:46 +0200

    add BookViewSet

--- books/views.py
+++ books/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from books.models import Book
+from books.serializers import BookSerializer, BookListSerializer
+
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BookListSerializer
+        return BookSerializer
