d88b41b4e9e8bcbf343d190ffd3da3b14e714dbd
Olena Zaritska <zaritskaos@gmail.com>
2024-11-01 21:00:42 +0200

    add url for user management

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from users.views import CreateUserView
+from users.views import CreateUserView, ManageUserView
 
 app_name = "users"
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path("", CreateUserView.as_view(), name="create"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
