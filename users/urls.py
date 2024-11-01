eecce915aa635ce90926dca6675cfc2677b75fbc
Olena Zaritska <zaritskaos@gmail.com>
2024-11-01 20:54:18 +0200

    add urls for user authorization

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.views import CreateUserView
 
@@ -6,4 +7,6 @@ app_name = "users"
 
 urlpatterns = [
     path("", CreateUserView.as_view(), name="create"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
