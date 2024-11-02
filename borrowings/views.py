6fde99f35f099c6aa8a2f1afe8285509a178b5f7
Olena Zaritska <zaritskaos@gmail.com>
2024-11-02 17:06:11 +0200

    add BorrowingViewSet with custom return_book action

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework import mixins, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import (
+    BorrowingDetailSerializer,
+    BorrowingListSerializer,
+    BorrowingSerializer,
+    BorrowingReturnSerializer,
+)
+
+
+class BorrowingViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Borrowing.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+        elif self.action == "list":
+            return BorrowingListSerializer
+        elif self.action == "create":
+            return BorrowingSerializer
+        return BorrowingReturnSerializer
+
+    @action(detail=True, methods=["post"], url_path="return")
+    def return_book(self, request, pk=None):
+        borrowing = self.get_object()
+        if borrowing.actual_return_date:
+            return Response(
+                {"detail": "This book has already been returned."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        borrowing.return_book()
+        return Response(
+            {"detail": "Book successfully returned."}, status=status.HTTP_200_OK
+        )
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
