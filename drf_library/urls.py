aa18160ed1ee1be7e4e5c1e83fdec1c30c950eae
Olena Zaritska <zaritskaos@gmail.com>
2024-11-06 10:05:00 +0200

    add documentation urls

--- drf_library/urls.py
+++ drf_library/urls.py
@@ -18,10 +18,26 @@ Including another URLconf
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("books/", include("books.urls", namespace="books")),
     path("users/", include("users.urls", namespace="users")),
     path("borrowings/", include("borrowings.urls", namespace="borrowings")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ] + debug_toolbar_urls()
